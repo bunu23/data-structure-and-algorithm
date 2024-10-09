5a83f8fea7b1e5dc7b29ba7d31cbefb74b77faa5
Bunu <bunubhattarai39@gmail.com>
2024-10-09 19:55:35 -0400

    level order traversal

---
+++ tree/LevelOrderTraversal.java
@@ -0,0 +1,63 @@
+// Level order traversal of a binary tree
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+
+    private TreeNode root;
+
+    private class TreeNode {
+        private TreeNode left;
+        private TreeNode right;
+        private int data; // Can be any generic type
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public void levelOrder() {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            System.out.print(temp.data);
+            if (temp.left != null) {
+                queue.offer(temp.left);
+            }
+            if (temp.right != null) {
+                queue.offer(temp.right);
+            }
+        }
+
+    }
+
+    public void createBinaryTree() {
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(42);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
+
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
+    }
+
+    public static void main(String[] args) {
+        LevelOrderTraversal bt = new LevelOrderTraversal();
+        bt.createBinaryTree();
+        bt.levelOrder();
+
+    }
+}
